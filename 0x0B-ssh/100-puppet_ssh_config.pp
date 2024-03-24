ce681c8ace22a0284cee83ac41a89ecc48b616fc
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-03-24 08:54:59 -0700

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# using Puppet to make changes to our configuration file.
+include stdlib
+
+file_line {'Turn off passwd auth':
+  ensure   => present,
+  path     => '/etc/ssh/ssh_config',
+  line     => '  PasswordAuthentication no',
+  replace  => true,
+}
+
+file_line {'Declare Identity File':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '  IdentityFile ~/.ssh/school',
+  replace => true,
+}
