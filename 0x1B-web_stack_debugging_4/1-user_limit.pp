4d4da7b31cea3b799c12218c81b4f4cabc936688
Eric Uwiringiyimana <uwiringiyimanaericmax@gmail.com>
2024-05-16 07:51:53 +0000

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,11 @@
 # change-os-configuration-for-holberton-user
 
-exec {'change-os-configuration-for-holberton-user':
-  command => 'sudo add user holberton',
+exec {'modify max open files limit setting':
+  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:bin/'
+}
+
+exec {'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:bin/'
 }
